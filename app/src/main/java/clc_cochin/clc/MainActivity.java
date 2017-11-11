b14034435c092cbc5f6c13fae2226116eb147891
Sibin Joseph <32307148+Sibinjoseph111@users.noreply.github.com>
2017-11-11 19:53:08 +0530

    Adding Login and navigation drawer

--- app/src/main/java/clc_cochin/clc/MainActivity.java
+++ app/src/main/java/clc_cochin/clc/MainActivity.java
@@ -1,13 +1,135 @@
 package clc_cochin.clc;
 
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.annotation.NonNull;
+import android.support.annotation.Nullable;
+import android.support.design.widget.NavigationView;
+import android.support.v4.app.FragmentTransaction;
+import android.support.v4.view.GravityCompat;
+import android.support.v4.widget.DrawerLayout;
+import android.support.v7.app.ActionBarDrawerToggle;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
+import android.view.Gravity;
+import android.view.MenuItem;
+
+import com.google.firebase.auth.FirebaseAuth;
 
 public class MainActivity extends AppCompatActivity {
-//dfsdfsdf
+
+    Toolbar toolbar;
+    DrawerLayout drawerLayout;
+    ActionBarDrawerToggle actionBarDrawerToggle;
+    NavigationView navigationView;
+    FragmentTransaction fragmentTransaction;
+    private boolean shouldLoadProductFragOnBackPress = false;
+    public static int navItemIndex = 0;
+    public FirebaseAuth mAuth;
+
+    private void Load_Product_fragment() {
+        navItemIndex = 0;
+        fragmentTransaction = getSupportFragmentManager().beginTransaction();
+        fragmentTransaction.replace(R.id.maincontainer,new ProductFragment());
+        fragmentTransaction.commit();
+        getSupportActionBar().setTitle(R.string.Productfragment_Title);
+        drawerLayout.closeDrawers();
+
+
+    }
+    private void Load_Service_fragment(){
+        navItemIndex = 1;
+        fragmentTransaction = getSupportFragmentManager().beginTransaction();
+        fragmentTransaction.replace(R.id.maincontainer,new ServiceFragment());
+        fragmentTransaction.commit();
+        getSupportActionBar().setTitle(R.string.Servicefragmnet_Title);
+        drawerLayout.closeDrawers();
+        shouldLoadProductFragOnBackPress = true;
+
+    }
+    private void Load_Account_fragment(){
+        navItemIndex = 2;
+        fragmentTransaction = getSupportFragmentManager().beginTransaction();
+        fragmentTransaction.replace(R.id.maincontainer,new AccountFragment());
+        fragmentTransaction.commit();
+        getSupportActionBar().setTitle(R.string.Accountfragment_Title);
+        drawerLayout.closeDrawers();
+        shouldLoadProductFragOnBackPress = true;
+
+    }
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        toolbar=(Toolbar)findViewById(R.id.Toolbar_Layout);
+        setSupportActionBar(toolbar);
+        drawerLayout=(DrawerLayout) findViewById(R.id.Drawer_Layout);
+        actionBarDrawerToggle = new ActionBarDrawerToggle(this,drawerLayout,toolbar,R.string.drawer_open,R.string.drawer_close);
+        drawerLayout.addDrawerListener(actionBarDrawerToggle);
+
+        drawerLayout.openDrawer(Gravity.LEFT);
+
+        Load_Product_fragment();
+
+        navigationView= findViewById(R.id.Navigation_View);
+        navigationView.setNavigationItemSelectedListener(new NavigationView.OnNavigationItemSelectedListener() {
+            @Override
+            public boolean onNavigationItemSelected(@NonNull MenuItem item) {
+                switch (item.getItemId())
+                {
+                    case R.id.Productsfragment_ND:
+                        Load_Product_fragment();
+                        item.setChecked(true);
+                        break;
+
+                    case R.id.Servicefragment_ND:
+                        Load_Service_fragment();
+                        item.setChecked(true);
+                        break;
+
+                    case R.id.Accountfragemnt_ND:
+                        Load_Account_fragment();
+                        item.setChecked(true);
+                        break;
+                }
+                return false;
+            }
+        });
     }
+
+    @Override
+    public void onBackPressed() {
+        if (drawerLayout.isDrawerOpen(GravityCompat.START)) {
+            drawerLayout.closeDrawers();
+            return;
+        }
+
+        // This code loads home fragment when back key is pressed
+        // when user is in other fragment than home
+        if (shouldLoadProductFragOnBackPress) {
+            // checking if user is on other navigation menu
+            // rather than home
+            if (navItemIndex !=0) {
+                navItemIndex = 0;
+                shouldLoadProductFragOnBackPress = false;
+                Load_Product_fragment();
+
+
+                return;
+            }
+        }
+
+        super.onBackPressed();
+    }
+
+
+
+
+    @Override
+    protected void onPostCreate(@Nullable Bundle savedInstanceState) {
+        super.onPostCreate(savedInstanceState);
+        actionBarDrawerToggle.syncState();
+    }
+
 }
+
--- app/src/main/java/clc_cochin/clc/ServiceFragment.java
+++ app/src/main/java/clc_cochin/clc/ServiceFragment.java
@@ -1,7 +1,5 @@
 package clc_cochin.clc;
 
-import android.content.Context;
-import android.net.Uri;
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
@@ -9,15 +7,8 @@ import android.view.View;
 import android.view.ViewGroup;
 
 
-/**
- * A simple {@link Fragment} subclass.
- * Activities that contain this fragment must implement the
- * {@link ServiceFragment.OnFragmentInteractionListener} interface
- * to handle interaction events.
- */
-public class ServiceFragment extends Fragment {
 
-    private OnFragmentInteractionListener mListener;
+public class ServiceFragment extends Fragment {
 
     public ServiceFragment() {
         // Required empty public constructor
@@ -31,42 +22,4 @@ public class ServiceFragment extends Fragment {
         return inflater.inflate(R.layout.fragment_service, container, false);
     }
 
-    // TODO: Rename method, update argument and hook method into UI event
-    public void onButtonPressed(Uri uri) {
-        if (mListener != null) {
-            mListener.onFragmentInteraction(uri);
-        }
-    }
-
-    @Override
-    public void onAttach(Context context) {
-        super.onAttach(context);
-        if (context instanceof OnFragmentInteractionListener) {
-            mListener = (OnFragmentInteractionListener) context;
-        } else {
-            throw new RuntimeException(context.toString()
-                    + " must implement OnFragmentInteractionListener");
-        }
-    }
-
-    @Override
-    public void onDetach() {
-        super.onDetach();
-        mListener = null;
-    }
-
-    /**
-     * This interface must be implemented by activities that contain this
-     * fragment to allow an interaction in this fragment to be communicated
-     * to the activity and potentially other fragments contained in that
-     * activity.
-     * <p>
-     * See the Android Training lesson <a href=
-     * "http://developer.android.com/training/basics/fragments/communicating.html"
-     * >Communicating with Other Fragments</a> for more information.
-     */
-    public interface OnFragmentInteractionListener {
-        // TODO: Update argument type and name
-        void onFragmentInteraction(Uri uri);
-    }
 }
