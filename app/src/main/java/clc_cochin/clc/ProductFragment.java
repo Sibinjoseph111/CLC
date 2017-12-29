93524de4984067e2e05084c52a2c01d2f1a9784d
shyamunnikrishnan <shyamunnikrishnan@users.noreply.github.com>
2017-12-29 13:28:23 +0530

    Recycler view

--- app/src/main/java/clc_cochin/clc/ProductFragment.java
+++ app/src/main/java/clc_cochin/clc/ProductFragment.java
@@ -1,17 +1,40 @@
 package clc_cochin.clc;
 
 
-import android.os.Bundle;
 import android.support.v4.app.Fragment;
+
+import android.content.Context;
+import android.os.Bundle;
+import android.support.annotation.NonNull;
+import android.support.v7.widget.DividerItemDecoration;
+import android.support.v7.widget.LinearLayoutManager;
+import android.support.v7.widget.RecyclerView;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+
+import com.google.android.gms.games.event.Event;
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.firestore.DocumentChange;
+import com.google.firebase.firestore.DocumentSnapshot;
+import com.google.firebase.firestore.EventListener;
+import com.google.firebase.firestore.FirebaseFirestore;
+import com.google.firebase.firestore.FirebaseFirestoreException;
+import com.google.firebase.firestore.QuerySnapshot;
+
+import java.util.ArrayList;
+import java.util.List;
 
 
 /**
  * A simple {@link Fragment} subclass.
  */
 public class ProductFragment extends Fragment {
+    private static final String TAG = "ProductFragment";
+    private FirebaseFirestore firestoreDB;
+    private RecyclerView productRecyclerView;
 
 
     public ProductFragment() {
@@ -23,7 +46,62 @@ public class ProductFragment extends Fragment {
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_product, container, false);
+
+        View view =  inflater.inflate(R.layout.fragment_product, container, false);
+        firestoreDB = FirebaseFirestore.getInstance();
+        productRecyclerView = (RecyclerView) view.findViewById(R.id.events_lst);
+
+        LinearLayoutManager recyclerLayoutManager =
+                new LinearLayoutManager(getActivity().getApplicationContext());
+        productRecyclerView.setLayoutManager(recyclerLayoutManager);
+
+        DividerItemDecoration dividerItemDecoration =
+                new DividerItemDecoration(productRecyclerView.getContext(),
+                        recyclerLayoutManager.getOrientation());
+        productRecyclerView.addItemDecoration(dividerItemDecoration);
+
+        getDocumentsFromCollection();
+
+        return view;
+
+    }
+    @Override
+    public void onAttach(Context context) {
+        super.onAttach(context);
     }
 
+
+    private void getDocumentsFromCollection() {
+        firestoreDB.collection("products").get()
+                .addOnCompleteListener(new OnCompleteListener<QuerySnapshot>() {
+                    @Override
+                    public void onComplete(@NonNull Task<QuerySnapshot> task) {
+                        if (task.isSuccessful()) {
+                            List<Event> eventList = new ArrayList<>();
+
+                            productRecyclerViewAdapter recyclerViewAdapter = new
+                                    productRecyclerViewAdapter(eventList,
+                                    getActivity(), firestoreDB);
+                            productRecyclerView.setAdapter(recyclerViewAdapter);
+
+                        } else {
+                            Log.d(TAG, "Error getting documents: ", task.getException());
+                        }
+                    }
+                });
+
+        firestoreDB.collection("products")
+                .addSnapshotListener(getActivity(), new EventListener<QuerySnapshot>() {
+                    @Override
+                    public void onEvent(QuerySnapshot documentSnapshots, FirebaseFirestoreException e) {
+                        for(DocumentChange doc : documentSnapshots.getDocumentChanges()){
+                            doc.getDocument().toObject(Event.class);
+                            //do something...
+                        }
+                    }
+                });
+    }
 }
+
+
+
---
+++ app/src/main/java/clc_cochin/clc/productRecyclerViewAdapter.java
@@ -0,0 +1,76 @@
+package clc_cochin.clc;
+
+import android.content.Context;
+import android.support.v4.app.FragmentActivity;
+import android.support.v7.widget.RecyclerView;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.TextView;
+
+import com.google.android.gms.games.event.Event;
+import com.google.firebase.firestore.FirebaseFirestore;
+
+import java.util.List;
+
+/**
+ * Created by Shyam PC on 29-12-2017.
+ */
+
+
+
+public class productRecyclerViewAdapter extends
+        RecyclerView.Adapter<productRecyclerViewAdapter.ViewHolder> {
+
+    private List<Event> eventsList;
+    private Context context;
+    private FirebaseFirestore firestoreDB;
+
+    public productRecyclerViewAdapter(List<Event> list, Context ctx, FirebaseFirestore firestore) {
+        eventsList = list;
+        context = ctx;
+        firestoreDB = firestore;
+    }
+
+    @Override
+    public int getItemCount() {
+        return eventsList.size();
+    }
+
+    @Override
+    public productRecyclerViewAdapter.ViewHolder
+    onCreateViewHolder(ViewGroup parent, int viewType) {
+
+        View view = LayoutInflater.from(parent.getContext())
+                .inflate(R.layout.list_layout, parent, false);
+
+        productRecyclerViewAdapter.ViewHolder viewHolder =
+                new productRecyclerViewAdapter.ViewHolder(view);
+        return viewHolder;
+    }
+
+    @Override
+    public void onBindViewHolder(productRecyclerViewAdapter.ViewHolder holder, int position) {
+        final int itemPos = position;
+        final Event event = eventsList.get(position);
+        holder.item_name.setText(event.getName());
+        holder.price.setText((int) event.getValue());
+
+
+    }
+
+    public class ViewHolder extends RecyclerView.ViewHolder {
+
+        public TextView item_name;
+        public TextView price;
+
+
+        public ViewHolder(View view) {
+            super(view);
+
+            item_name = view.findViewById(R.id.List_maintext);
+            price = view.findViewById(R.id.List_subtext);
+
+        }
+    }
+}
